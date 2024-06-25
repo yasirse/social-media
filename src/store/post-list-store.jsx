eb64b5d104c1da1fb0acc56211750ebb9b057464
Yasir Arfat <yasir.arfatse@gmail.com>
2024-06-25 07:42:57 +0500

    Default value for Reducer

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,6 +1,21 @@
-import{createcontext} from react
-const PostList=createcontext({});
+import{createContext,useReducer} from "react"
+
+
+const PostList=createContext({postList:[],addPost:()=>{},deletePost:()=>{}});
+
+const postListReducer=(currPostList,action)=>
+{
+    return currPostList;
+}
 const PostListProvider=({children})=>{
-    return <PostList.PostListProvider>{children}</PostList.PostListProvider>;
+    const {postList,dispatchPostList}=useReducer(postListReducer,[]);
+   
+    const addPost=()=>{    }
+    const deletePost=()=>{     }
+
+    return(
+    <PostList.Provider value={{postList,addPost,deletePost}}>{children}
+    </PostList.Provider>
+    );
 }
 export default PostListProvider;
