c3c80aa1dc9b1c85abf1ecde562b2c7b45e7b03e
Yasir Arfat <yasir.arfatse@gmail.com>
2024-06-28 10:31:23 +0500

    welcom message

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import Post from "./Post";
 import {PostList as PostListData} from "../store/post-list-store"
+import WelcomeMessage from "./Welcome";
 
 const PostList=()=>{
     const {postList}=useContext(PostListData);
     return(
         <>
-        {postList.map((post)=>{return(<Post key={post.id} post={post}/>)})}
-        
+        {postList.length===0&&<WelcomeMessage/>}
+        {postList.map((post)=>{return(<Post key={post.id} post={post}/>)})}        
         </>
 
     );
